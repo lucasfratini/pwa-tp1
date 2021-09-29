b56b1a2b79908adadb64279b7b7d0491e49a56a2
fratinilucas <matias_anubis@hotmail.com>
2021-09-29 10:59:06 -0300

    punto2

---
+++ src/basic/config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host='.$_ENV['DDBB_HOST'].';dbname='.$_ENV['DDBB_NAME'],
+    'username' =>$_ENV['DDBB_USER'],
+    'password' => $_ENV['DDBB_PASSWORD'],
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
+
