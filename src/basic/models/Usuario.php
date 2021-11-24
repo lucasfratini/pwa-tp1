f1b423d4482c2b4507308b5abbce98c37c5dc5d3
fratinilucas <matias_anubis@hotmail.com>
2021-11-24 11:00:12 -0300

    punto 6 7 8 se implemento la clase identityinterface y las funciones solicitadas 7 se adapto el models/loginForm y en el 8 yii para nuevo modelo

--- src/basic/models/Usuario.php
+++ src/basic/models/Usuario.php
@@ -15,7 +15,7 @@ use Yii;
  * @property string $accessToken
  * @property string $authKey
  */
-class Usuario extends \yii\db\ActiveRecord
+class Usuario extends \yii\db\ActiveRecord  implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -52,5 +52,37 @@ class Usuario extends \yii\db\ActiveRecord
             'accessToken' => 'Access Token',
             'authKey' => 'Auth Key',
         ];
+    }
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::findOne(['accessToken'=>$token]);
+    }
+    public static function findByUsername($username)
+    {
+        return self::findOne(['username'=>$username]);
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+   public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    public function validatePassword($password)
+    {
+        return password_verify($password, $this->password);
     }
 }
