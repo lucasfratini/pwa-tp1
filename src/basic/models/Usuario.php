ed4a146992c03d0d257b2d21c1aa0b2beadf9d64
fratinilucas <matias_anubis@hotmail.com>
2021-11-24 09:08:01 -0300

    punto2: configuracion de web y modelado de tabla usuario con su ABM

---
+++ src/basic/models/Usuario.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuario".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $nombre
+ * @property string $apellido
+ */
+class Usuario extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'usuario';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'nombre', 'apellido'], 'required'],
+            [['username'], 'string', 'max' => 50],
+            [['nombre', 'apellido'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'nombre' => 'Nombre',
+            'apellido' => 'Apellido',
+        ];
+    }
+}
